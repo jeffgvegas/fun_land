670973f815439e9debcbc62cd51f8a05c902026f
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-11-23 23:41:07 -0800

    guests table completed

--- seed.rb
+++ seed.rb
@@ -2,7 +2,12 @@
 
 require "faker"
  
-names = Faker::Name.name 
-File.open("guest_seed.txt", "w+")
+File.open("guest_seed.txt", "w+") do |file|
+	250.times do |i|
+	name = Faker::Name.name
+	tokens = Random.rand(10000)
+	file.write("INSERT INTO guests (name, tokens) VALUES ('#{name}',#{tokens});\n")
 
-end 
+	end 
+end
+ 
