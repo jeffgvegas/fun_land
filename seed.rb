f68044e2af60a7c38295e1ef14bf74ba7564f238
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-11-24 00:54:35 -0800

    rewards_seed added

--- seed.rb
+++ seed.rb
@@ -2,12 +2,19 @@
 
 require "faker"
  
-File.open("guest_seed.txt", "w+") do |file|
+ File.open("guest_seed.txt", "w+") do |file|
 	250.times do |i|
 	name = Faker::Name.name
 	tokens = Random.rand(10000)
 	file.write("INSERT INTO guests (name, tokens) VALUES ('#{name}',#{tokens});\n")
-
-	end 
-end
+		end 
+	end
  
+File.open("rewards_seed.txt", "w+") do |file|
+	75.times do |i|
+	item_name = Faker::Commerce.product_name
+	tokens = Random.rand(5..5000)
+	file.write("INSERT INTO rewards (item_name, token_cost) VALUES ('#{item_name}',#{tokens});\n")
+		end
+	end
+
