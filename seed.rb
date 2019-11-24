b6489b30559b7f8050a6ef0ba2543a74ecf1cc02
Jeffery Gontarek <jeffery@Jefferys-MacBook-Pro.local>
2019-11-24 01:15:32 -0800

    added availabity column in rewards table

--- seed.rb
+++ seed.rb
@@ -14,7 +14,8 @@ File.open("rewards_seed.txt", "w+") do |file|
 	75.times do |i|
 	item_name = Faker::Commerce.product_name
 	tokens = Random.rand(5..5000)
-	file.write("INSERT INTO rewards (item_name, token_cost) VALUES ('#{item_name}',#{tokens});\n")
+	availability = [true, true, true, false].sample
+	file.write("INSERT INTO rewards (item_name, token_cost, in_stock) VALUES ('#{item_name}',#{tokens},#{availability});\n")
 		end
 	end
 
